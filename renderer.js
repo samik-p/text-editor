b5324d1ba0b8283398c1f673309f27c3dab93835
Samik Pradhan <samikpradhan@gmail.com>
2024-09-11 19:54:10 -0700

    created a canvas and tested preload scripts

---
+++ renderer.js
@@ -0,0 +1,3 @@
+const info = document.getElementById('info');
+
+info.innerText = `This app is using Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), and Electron (v${versions.electron()})`
